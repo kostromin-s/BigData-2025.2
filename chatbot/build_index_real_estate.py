dd32ed3a3ad96faab7e2cbff0af4104e6ae7b6f5
kostromin-s <nam.lh220118@sis.hust.edu.vn>
2026-06-06 23:16:59 +0700

    upcode

--- chatbot/build_index_real_estate.py
+++ chatbot/build_index_real_estate.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from qdrant_client import QdrantClient
 from langchain_qdrant import QdrantVectorStore
